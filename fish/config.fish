9a8c784a8ed3d0c23c0c6cdcb4f4f54a8a6f95fe
KhanhTIMN <khanhtimn@Quangs-MacBook-Air.local>
2024-01-07 14:49:15 +0700

    Cleanup

--- fish/config.fish
+++
@@ -1,9 +0,0 @@
-set -gx VISUAL nvim
-set -gx EDITOR $VISUAL
-
-
-oh-my-posh init fish --config ~/.config/oh-my-posh/themes/bubblesline.omp.json | source
-
-function fish_greeting
-    pokemon-colorscripts -r
-end
