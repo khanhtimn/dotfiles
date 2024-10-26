f0ec24d91a53a3ece59d41897bfcf82837cab011
khanhtimn <khanhlcbb@gmail.com>
2024-10-26 13:08:29 +0700

    stupid me commiting symlinks

---
+++ fish/config.fish
@@ -0,0 +1,21 @@
+set fish_greeting
+
+set VISUAL nvim
+set EDITOR $VISUAL
+
+fish_vi_key_bindings
+
+# List Directory
+alias ls="lsd"
+alias l="ls -l"
+alias la="ls -a"
+alias lla="ls -la"
+alias lt="ls --tree"
+# Handy change dir shortcuts
+abbr .. 'cd ..'
+abbr ... 'cd ../..'
+abbr .3 'cd ../../..'
+abbr .4 'cd ../../../..'
+abbr .5 'cd ../../../../..'
+# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
+abbr mkdir 'mkdir -p'
