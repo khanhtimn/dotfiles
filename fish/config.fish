43b8dd3c0141eef1402b41448a899d209a129615
khanhtimn <khanhlcbb@gmail.com>
2024-10-29 13:33:27 +0700

    macchina kinda kinda..

--- fish/config.fish
+++ fish/config.fish
@@ -14,6 +14,8 @@ alias l="ls -l"
 alias la="ls -a"
 alias lla="ls -la"
 alias lt="ls --tree"
+alias neofetch="macchina"
+alias fetch="macchina"
 # Handy change dir shortcuts
 abbr .. 'cd ..'
 abbr ... 'cd ../..'
